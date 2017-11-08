62027de528df9627eee926887bb61f31fa1f2e60
Jacob Gaffney <jacobgaffney@me.com>
2017-11-09 00:34:10 +1100

    Initial Commit

---
+++ ToastKit/ToastKit.h
@@ -0,0 +1,19 @@
+//
+//  ToastKit.h
+//  ToastKit
+//
+//  Created by jacques on 9/11/17.
+//  Copyright © 2017 Jacob Gold. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ToastKit.
+FOUNDATION_EXPORT double ToastKitVersionNumber;
+
+//! Project version string for ToastKit.
+FOUNDATION_EXPORT const unsigned char ToastKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastKit/PublicHeader.h>
+
+
