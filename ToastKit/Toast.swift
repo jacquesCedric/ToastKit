15804593dd9587b682b58101f1634a22dc481e71
Jacob Gaffney <jacobgaffney@me.com>
2017-11-10 00:01:47 +1100

    Added new notification type, with image and title Cleaned up code a little (attempt to improve flow)

--- ToastKit/Toast.swift
+++ ToastKit/Toast.swift
@@ -1,6 +1,6 @@
 //
-//  Toast.swift
-//  ToastKit
+//  toast.swift
+//  toastKit
 //
 //  Created by Jacob Gold on 8/11/17.
 //  Copyright © 2017 Jacob Gold. All rights reserved.
@@ -10,31 +10,33 @@ import Cocoa
 
 // MARK: - External call
 extension NSViewController {
-    public func toast(message: String, fade: CFTimeInterval = 1.8) {
-        let toast = makeToast(message: message)
-        self.view.addSubview(toast)
-        
-        let vConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-(>=20)-[toast]-20-|", options: .alignAllBottom, metrics: nil, views: ["toast" : toast])
-        NSLayoutConstraint.activate(vConstraints)
-        toast.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        
-        animateToast(toast, fade: fade)
+    public func toast(message: String, title: String, image: NSImage = NSImage(named: NSImage.Name.caution)!) {
+        let t = makeToast(message: message, title: title, image: image)
+        handleToastForDisplay(toast: t)
+    }
+    
+    public func toast(message: String) {
+        let t = makeToast(message: message)
+        handleToastForDisplay(toast: t)
     }
 }
 
 //////////////////////////////////////////////
 // MARK: - Internal funcs
 fileprivate extension NSViewController {
-    // Generate the message component of the toast
-    func createTextLabel(message: String) -> NSTextField {
-        let tf = NSTextField(frame: NSMakeRect(0, 0, 200, 17))
-        tf.stringValue = message
-        let stf = styleTextLabel(tf: tf)
+    // This function adds the toast to the view calling it and then dismisses it when necessary
+    func handleToastForDisplay(toast: NSView) {
+        self.view.addSubview(toast)
         
-        return stf
+        let vConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-(>=20)-[toast]-20-|", options: .alignAllBottom, metrics: nil, views: ["toast" : toast])
+        NSLayoutConstraint.activate(vConstraints)
+        toast.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        animateToastFade(toast)
     }
     
-    func makeToast(message: String) -> NSView{
+    // Standard toast type, with message
+    func makeToast(message: String) -> NSView {
         let v = NSView()
         // Styling
         v.wantsLayer = true;
@@ -43,11 +45,11 @@ fileprivate extension NSViewController {
         // Add message
         let m = createTextLabel(message: message)
         v.addSubview(m)
-        //        v.frame = m.frame
+        
+        // Constrain the views
         m.translatesAutoresizingMaskIntoConstraints = false
         v.translatesAutoresizingMaskIntoConstraints = false
         
-        // Constrain the views
         let hConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-5-[m]-5-|", options: .alignAllTop, metrics: nil, views: ["m" : m])
         NSLayoutConstraint.activate(hConstraints)
         let vConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[m]-5-|", options: .alignAllTop, metrics: nil, views: ["m" : m])
@@ -60,6 +62,57 @@ fileprivate extension NSViewController {
         return v
     }
     
+    // Toast type with image and title
+    func makeToast(message: String, title: String, image: NSImage) -> NSView {
+        let v = NSView()
+        // Styling
+        v.wantsLayer = true;
+        v.layer = styleToast()
+        
+        // Add message
+        let m = createTextLabel(message: message, alignment: .left)
+        v.addSubview(m)
+        
+        // Add the title
+        let t = createTextLabel(message: title, alignment: .left)
+        v.addSubview(t)
+        
+        // Add an image
+        image.size = NSSize(width: 50.0, height: 50.0)
+        let i = NSImageView(image: image)
+        v.addSubview(i)
+        
+        // Setting constraints
+        m.translatesAutoresizingMaskIntoConstraints = false
+        t.translatesAutoresizingMaskIntoConstraints = false
+        i.translatesAutoresizingMaskIntoConstraints = false
+        v.translatesAutoresizingMaskIntoConstraints = false
+        
+        let h1Constraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[i(50)]-5-[t(>=200,<=300)]-5-|", options: .alignAllTop, metrics: nil,
+                                                           views: ["t" : t, "i" : i])
+        let h2Constraints = NSLayoutConstraint.constraints(withVisualFormat: "H:[m(>=200,<=300)]", options: .alignAllTop, metrics: nil,
+                                                           views: ["m" : m])
+        let v1Constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[t(17)]-1-[m(>=17)]-5-|", options: .alignAllLeft, metrics: nil,
+                                                           views: ["t" : t, "m" : m])
+        let v2Constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-(>=5)-[i(50)]-(>=5)-|", options: .alignAllLeft, metrics: nil,
+                                                           views: ["i" : i])
+        NSLayoutConstraint.activate(h1Constraints)
+        NSLayoutConstraint.activate(h2Constraints)
+        NSLayoutConstraint.activate(v1Constraints)
+        NSLayoutConstraint.activate(v2Constraints)
+        //        i.centerYAnchor.constraint(equalTo: v.centerYAnchor).isActive = true
+        
+        return v
+    }
+    
+    // Generate the message component of the toast
+    func createTextLabel(message: String, alignment: NSTextAlignment = .center) -> NSTextField {
+        let tf = NSTextField(frame: NSMakeRect(0, 0, 200, 17))
+        tf.stringValue = message
+        let stf = styleTextLabel(tf: tf, alignment: alignment)
+        
+        return stf
+    }
     
     //////////////////////////////////////////////
     // MARK: - Aesthetics
@@ -69,13 +122,13 @@ fileprivate extension NSViewController {
         let toastLayer = CALayer()
         toastLayer.backgroundColor = NSColor.black.withAlphaComponent(0.8).cgColor
         toastLayer.cornerRadius = 8
-        toastLayer.opacity = 0.8
+        toastLayer.opacity = 0.0
         
         return toastLayer
     }
     
     // Style the message text
-    func styleTextLabel(tf: NSTextField) -> NSTextField {
+    func styleTextLabel(tf: NSTextField, alignment: NSTextAlignment) -> NSTextField {
         // Sizing
         var f = tf.frame
         f.size.height = tf.intrinsicContentSize.height
@@ -88,25 +141,25 @@ fileprivate extension NSViewController {
         tf.focusRingType = .none
         tf.isEditable = false
         tf.isSelectable = false
-        tf.alignment = .center
+        tf.alignment = alignment
         
         return tf
     }
     
     // Animate fade out
-    func animateToast(_ toast: NSView, fade: CFTimeInterval) {
-        
-        let animation = CABasicAnimation(keyPath: "opacity")
-        animation.fromValue = 0.8
-        animation.toValue = 0.0
-        animation.beginTime = CACurrentMediaTime() + 1.0
-        animation.duration = fade
+    func animateToastFade(_ toast: NSView) {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "opacity"
+        animation.values = [0, 0.8, 0.8, 0]
+        animation.keyTimes = [0, 0.01, 0.8, 1]
+        animation.duration = 3.0
+        animation.isAdditive = true
         
         CATransaction.begin()
         CATransaction.setCompletionBlock {
             toast.removeFromSuperview()
         }
-        toast.layer?.add(animation, forKey: "fadeout")
+        toast.layer?.add(animation, forKey: "opacity")
         CATransaction.commit()
     }
 }
